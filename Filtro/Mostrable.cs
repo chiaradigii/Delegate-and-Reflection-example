a54e7bfa0e6553955eff651fe7e963a35c1f41c3
Chiara Di Giannantonio <77933300+chiaradigii@users.noreply.github.com>
2023-06-15 10:05:04 -0300

    Update Mostrable.cs

--- Filtro/Mostrable.cs
+++ Filtro/Mostrable.cs
@@ -6,13 +6,11 @@ using System.Threading.Tasks;
 
 namespace Filtro
 {
-    //para que sea personalizado debe tener ese[System.AttributeUsage(System.AttributeTargets.CLass)] → ahi le puse que se pueda usar en CLASES.Podria ser otra cosa como Structs, o ambas
-    //dice ese attribute que se puede usar a nivel de Clases porque yo quiero etiquetar clases en este caso para identificarlas de otras clases para filtrar solo las que tienen el CUstom Attribute mostrable
 
          //Custom Attribute
          [System.AttributeUsage(System.AttributeTargets.Class)]
 
-        public class Mostrable : System.Attribute  //heredo de Attribute
+        public class Mostrable : System.Attribute 
         {
             string leyenda;
             public Mostrable(string pleyenda) 
