40afd8c571c9546a1d7b2769ffb0271945887489
Chiara Di Giannantonio <77933300+chiaradigii@users.noreply.github.com>
2023-06-15 10:05:55 -0300

    Update FiltroIgualQue.cs

--- Filtro/FiltroIgualQue.cs
+++ Filtro/FiltroIgualQue.cs
@@ -14,11 +14,7 @@ namespace Filtro
         
             public override IEnumerable<string> Filtrar(string[] pArray, int pCorte)
             {
-                return pArray.Where<string>(x => x.Length == pCorte);  //Uso Where porq era una que daba IEnumerable
-                                                                       //x representa cada string en ese array de string
-                                                                       //a la derecha del lamda hay una propociocion para filtrar (devuelve verdadero si x.Length >=pCorte)
-                                                                       //recibe un string, devuelve un booleano
-                                                                       //los elementos que va aiterar el IEnumerable son en base a ese filtro que puse
+                return pArray.Where<string>(x => x.Length == pCorte);  
             }
         
 
